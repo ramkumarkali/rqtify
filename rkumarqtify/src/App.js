c0fe787cd0dab77ffdf535665f03ca68776b5d12
Your Name <you@example.com>
2024-06-03 23:45:19 +0530

    hero section

--- rkumarqtify/src/App.js
+++ rkumarqtify/src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Navbar from './components/navbar/Navbar';
+import Hero from './components/Hero/Hero';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <Hero />
     </div>
   );
 }
